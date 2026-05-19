94e610e195cd216bc08d6e02e0713b3029819a2b
tiagoworking5 <tiagoworking5@gmail.com>
2026-05-19 20:29:36 -0300

    Atividade 3 pag. 50 - python

---
+++ calculo_principal.py
@@ -0,0 +1,4 @@
+from math import pi
+raio = float (input("Digite o raio da área: "))
+area = pi * raio ** 2 # mesmo que area = pi * pow(raio, 2)
+print (f"Área = {area: .2f} .")
---
+++ convivencia.py
@@ -0,0 +1,6 @@
+
+from math import sqrt
+numero = float(input("Digite um número: "))
+raiz = sqrt(numero)
+print(f"A raiz quadrada é: {raiz}")
+
